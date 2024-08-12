496515032da4f7793f2ec3a0aaafb8f239decb1f
GabrielSTz <161839522+GabrielSTz@users.noreply.github.com>
2024-08-12 16:42:33 -0300

    Update 1.1.0
    ADD HIGHSCORE
    ADD END

--- game.js
+++ game.js
@@ -1,15 +1,14 @@
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('#choices-text'));
+const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
-const progressbarFull = document.querySelector('#progressbarFull');
+const progressBarFull = document.querySelector('#progressbarFull');
 
-
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
 let questions = [
     {
@@ -18,7 +17,7 @@ let questions = [
         choice2: '10',
         choice3: '21',
         choice4: '13',
-        answer: 13,
+        answer: 4,
     },
     {
         question: 'Quantos é 19 + 23?',
@@ -26,7 +25,7 @@ let questions = [
         choice2: '21',
         choice3: '42',
         choice4: '43',
-        answer: 42,
+        answer: 3,
     },
     {
         question: 'Quantos é 25 + 30 x 2?',
@@ -34,7 +33,7 @@ let questions = [
         choice2: '95',
         choice3: '71',
         choice4: '85',
-        answer: 85,
+        answer: 4,
     },
     {
         question: 'Quantos é 95 x 32 ÷ 4?',
@@ -42,34 +41,70 @@ let questions = [
         choice2: '844',
         choice3: '1049',
         choice4: '760',
-        answer: 760,
+        answer: 4,
     },
-]
+];
 
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 4
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 4;
 
 startGame = () => {
-    questionCounter = 0
-    score = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
-}
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS)
-        localStorage.setItem('mostRecentScore', score)
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+        return window.location.assign('/end.html');
+    }
 
-    return window.location.assign('/end.html')
-}
+    questionCounter++;
+    progressText.innerText = `Questão ${questionCounter} de ${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    qustionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressbarFull.style.width = `${(questionCounter/MAX_QUESTIONS) = 100}%`
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question;
 
-    const questionsIndex = Math.floor(Math.random() = availableQuestions.length)
-    currentQuestion = availableQuestions(questionsIndex)
-    question.innerText = currentQuestion.question
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
 
-    
+    availableQuestions.splice(questionsIndex, 1);
 
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset.number;
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if (classToApply === 'correct') {
+            incrementScore(SCORE_POINTS);
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    });
+});
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+};
+
+startGame();
